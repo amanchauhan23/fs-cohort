8b70751a2d82a640495ad9c28d3c939b012b6944
aman <amankch2023@gmail.com>
2023-07-04 02:35:22 +0530

    Week 2 done ✅

---
+++ week-02/authenticationServer.js
@@ -0,0 +1,98 @@
+/**
+  You need to create a HTTP server in Node.js which will handle the logic of an authentication server.
+  - Don't need to use any database to store the data.
+  - Save the users and their signup/login data in an array in a variable
+  - You can store the passwords in plain text (as is) in the variable for now
+
+  The expected API endpoints are defined below,
+  1. POST /signup - User Signup
+    Description: Allows users to create an account. This should be 
+    stored in an array on the server, and a unique id should
+    be generated for every new user that is added.
+    Request Body: JSON object with username, password, firstName and 
+    lastName fields.
+    Response: 201 Created if successful, or 400 Bad Request if the 
+    username already exists.
+    Example: POST http://localhost:3000/signup
+
+  2. POST /login - User Login
+    Description: Gets user back their details like firstName, 
+    lastName and id
+    Request Body: JSON object with username and password fields.
+    Response: 200 OK with an authentication token in JSON format if 
+    successful, or 401 Unauthorized if the credentials are invalid.
+    Example: POST http://localhost:3000/login
+
+  3. GET /data - Fetch all user's names and ids from the server (Protected route)
+    Description: Gets details of all users like firstName, lastName and id in an array format.
+     Returned object should have a key
+     called users which contains the list of all users with their email/firstName/lastName.
+    The users username and password should be fetched from the headers and checked before the array is returned
+    Response: 200 OK with the protected data in JSON format if the username and password in headers are valid, 
+    or 401 Unauthorized
+     if the username and password are missing or invalid.
+    Example: GET http://localhost:3000/data
+
+  - For any other route not defined in the server return 404
+
+  Testing the server - run `npm run test-authenticationServer` command in terminal
+ */
+
+const express = require("express");
+const PORT = 3000;
+const app = express();
+const crypto = require("crypto");
+app.use(express.json());
+
+let secret = "secret key";
+let data = [];
+
+app.post("/signup", (req, res) => {
+  let { email, password, firstName, lastName } = req.body;
+  let found = data.find((user) => user.email === email);
+  if (found) return res.status(400).send();
+  let newuser = {
+    id: crypto.randomUUID(),
+    email: email,
+    password: password,
+    firstName: firstName,
+    lastName: lastName,
+  };
+  data.push(newuser);
+  res.status(201).send("Signup successful");
+});
+
+app.post("/login", (req, res) => {
+  let { email, password } = req.body;
+  for (let i = 0; i < data.length; i++) {
+    if (data[i].email === email && data[i].password === password) {
+      found = data[i];
+      break;
+    }
+  }
+  if (!found) return res.status(401).send("user not found");
+  res.json({
+    firstName: found.firstName,
+    lastName: found.lastName,
+    email: found.email,
+  });
+});
+
+app.get("/data", (req, res) => {
+  let { email, password } = req.headers;
+  let found = data.find(
+    (user) => user.email === email && user.password === password
+  );
+  if (!found) return res.status(401).send("Unauthorized");
+  res.send({ users: data });
+});
+
+app.use((req, res) => {
+  res.status(404).send("Not found");
+});
+
+// app.listen(PORT, () => {
+//   console.log(`Listening on port no: ${PORT}`);
+// });
+
+module.exports = app;
